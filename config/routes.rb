ec173a52ae5588800c95b70d44931199eaa4866f
Talha affan siddique <talhaaffansiddique@gmail.com>
2020-10-09 20:43:22 +0500

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 #  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :delete]
-resources :articles
+  resources :articles
+  get 'signup', to: 'users#new'
+#  post  'users', to: 'users#create'
+  resources :users, except:[:new]
 end
